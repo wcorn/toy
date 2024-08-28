f2ea16f4d981ca811fae981a61c31c61da8fc1e2
Dongseok Kang <dsk08208@gmail.com>
2024-08-29 01:32:25 +0900

    Feat: Database 연결 (#16)
    mysql,h2 종속성 추가
    data jpa 종속성 추가
    mysql 설정 추가
    mysql profile 분리
    data jpa config 설정
    redis 종속성 추가
    redis config(redistemplate), util 추가
    redis 프로필 분리
    redis 설정 추가
    #15

---
+++ src/main/java/ds/project/toy/global/config/JPAAuditingConfig.java
@@ -0,0 +1,10 @@
+package ds.project.toy.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JPAAuditingConfig {
+
+}
