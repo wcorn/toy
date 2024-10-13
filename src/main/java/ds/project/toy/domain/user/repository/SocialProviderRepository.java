7b06d10b906690dbd76dae5835d2487a120b15f2
wcorn <dsk08208@gmail.com>
2024-10-13 12:54:08 +0900

    Feat: 소셜 로그인 provider 조회
    -  provider 조회
     #23

--- src/main/java/ds/project/toy/domain/user/repository/SocialProviderRepository.java
+++ src/main/java/ds/project/toy/domain/user/repository/SocialProviderRepository.java
@@ -1,10 +1,15 @@
 package ds.project.toy.domain.user.repository;
 
 import ds.project.toy.domain.user.entity.SocialProvider;
+import ds.project.toy.domain.user.vo.SocialProviderState;
+import ds.project.toy.global.common.vo.OAuth2Provider;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SocialProviderRepository extends JpaRepository<SocialProvider, String> {
 
+    Optional<SocialProvider> findByProviderAndState(OAuth2Provider provider,
+        SocialProviderState socialProviderState);
 }
