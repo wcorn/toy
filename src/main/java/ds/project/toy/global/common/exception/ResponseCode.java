c4c13f7d807bb65d47fb279d70bdf13417413724
wcorn <dsk08208@gmail.com>
2024-10-13 12:38:10 +0900

    Feat: 401 에러를 위한 ResponseCode 추가
     #23

--- src/main/java/ds/project/toy/global/common/exception/ResponseCode.java
+++ src/main/java/ds/project/toy/global/common/exception/ResponseCode.java
@@ -4,18 +4,19 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ResponseCode {
+    //AUTH
+    UNAUTHORIZED("AUTH-ERR-001", "인증되지 않은 사용자입니다."),
+
     //GLOBAL
-    BAD_REQUEST("GLB-ERR-001",  "잘못된 요청입니다."),
+    BAD_REQUEST("GLB-ERR-001", "잘못된 요청입니다."),
     METHOD_NOT_ALLOWED("GLB-ERR-002", "허용되지 않은 메서드입니다."),
     INTERNAL_SERVER_ERROR("GLB-ERR-003", "내부 서버 오류입니다.");
     private final String code;
     private final String message;
-
 
     public String getMessage(Throwable e) {
         return this.getMessage(this.getMessage() + " - " + e.getMessage());
