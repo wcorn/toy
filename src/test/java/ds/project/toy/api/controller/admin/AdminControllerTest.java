98c0911ea7ce702f2eca693986c1ddc92144a047
wcorn <dsk08208@gmail.com>
2024-11-30 09:20:40 +0900

    Test: 관리자 카테고리 등록 test code 작성
    - controller, service test 작성
     #72

--- src/test/java/ds/project/toy/api/controller/admin/AdminControllerTest.java
+++ src/test/java/ds/project/toy/api/controller/admin/AdminControllerTest.java
@@ -10,15 +10,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import ds.project.toy.ControllerTestSupport;
 import ds.project.toy.api.controller.admin.dto.request.AdminLoginRequest;
+import ds.project.toy.api.controller.admin.dto.request.PostCategoryRequest;
 import ds.project.toy.api.controller.admin.dto.response.GetCategoryResponse;
+import ds.project.toy.api.controller.admin.dto.response.PostCategoryResponse;
 import ds.project.toy.domain.product.vo.CategoryState;
 import ds.project.toy.global.common.vo.AuthToken;
 import java.util.Collections;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-
 
 class AdminControllerTest extends ControllerTestSupport {
 
@@ -63,5 +63,26 @@ class AdminControllerTest extends ControllerTestSupport {
                 jsonPath("$[0].categoryName").value(response.getCategoryName()),
                 jsonPath("$[0].categoryState").value(response.getCategoryState())
             );
+    }
+
+    @DisplayName(value = "관리자가 카테고리를 등록한다.")
+    @Test
+    void postCategory() throws Exception {
+        //given
+        PostCategoryRequest request = PostCategoryRequest.of("전자기기");
+        PostCategoryResponse response = PostCategoryResponse.of(1L);
+        given(adminService.postCategory(any())).willReturn(response);
+        //when
+        mockMvc.perform(
+                post("/admin/product/category")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON).with(csrf())
+            )
+            .andExpect(status().isOk())
+            .andExpectAll(
+                jsonPath("$.categoryId").value(response.getCategoryId())
+            );
+        //then
+
     }
 }
