e18c46c9bf1ce36b64957774f26949573ca8a0a3
wcorn <dsk08208@gmail.com>
2024-12-05 18:29:46 +0900

    Feat: 관심 제품 등록 API
     #65

--- src/main/java/ds/project/toy/api/service/product/ProductService.java
+++ src/main/java/ds/project/toy/api/service/product/ProductService.java
@@ -3,6 +3,7 @@ package ds.project.toy.api.service.product;
 import ds.project.toy.api.controller.product.dto.response.GetProductResponse;
 import ds.project.toy.api.controller.product.dto.response.PostProductResponse;
 import ds.project.toy.api.service.admin.dto.GetProductServiceDto;
+import ds.project.toy.api.service.product.dto.PostInterestProductServiceDto;
 import ds.project.toy.api.service.product.dto.PostProductServiceDto;
 
 public interface ProductService {
@@ -10,4 +11,6 @@ public interface ProductService {
     PostProductResponse postProduct(PostProductServiceDto of);
 
     GetProductResponse getProduct(GetProductServiceDto of);
+
+    void postInterestProduct(PostInterestProductServiceDto of);
 }
